6fddeca0c7d85ab47af59d1b7fcfe7429e15c8e4
Ashutosh <ashusighrajput88@gmail.com>
2025-07-01 23:57:28 +0530

    Created and renamed input of folders

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const box = document.querySelector('.dragbox')
 const view = document.getElementById('desktop')
 const menuBar = document.querySelector('.menuBar')
@@ -58,6 +60,39 @@ newFolder.addEventListener('click', () => {
     folderName.placeholder = "New Folder"
     const folderArea = document.createElement('div');
     const folder = document.createElement('div');
+
+    const hoverText = document.createElement("div");
+    hoverText.className = ".hover-text"
+    hoverText.textContent = ' Date created: 6/30/2025 2:05PM  Empty folder'
+    hoverText.style.cssText =
+        `   
+    position: absolute;
+    display: none;
+    padding: .1rem .5rem;
+    background: #ffffff;
+    font-size: #000;
+    font-weight: 100;
+    cursor: default;
+    width: 230px;
+    z-index: 1;
+    transition: all 1s ease-in-out;
+    `;
+
+
+    folder.appendChild(hoverText)
+
+    folder.addEventListener('mouseenter', (e) => {
+        hoverText.style.display = "block"
+         hoverText.style.top = `${e.clientY}px`
+    hoverText.style.left = `${e.clientX}px`
+
+    })
+    folder.addEventListener('mouseleave', () => {
+        hoverText.style.display = "none"
+
+    })
+
+
     // if (!folderName) return;
     folderArea.className = "folderArea";
     folder.className = "folder";
@@ -92,12 +127,57 @@ newFolder.addEventListener('click', () => {
 });
 
 
-function saveFolderName(folderName) {
-    const name = folderName.value.trim() || "New Folder"
 
+// this is the function for save the folder name......
+
+function saveFolderName(folderName) {
     const nameLabel = document.createElement("div");
     nameLabel.className = "folder-name"
 
+
+    const name = folderName.value.trim() || "New Folder"
+
+
     nameLabel.textContent = name;
+
+    nameLabel.addEventListener('click', () => {
+        enableRename(nameLabel)
+    })
+
     folderName.replaceWith(nameLabel)
+
+
+
+
+}
+
+
+// function for raname the folder......
+
+function enableRename(nameLabel) {
+    const currentName = nameLabel.textContent;
+
+    const renameInput = document.createElement('input');
+    renameInput.className = "folderName"
+    renameInput.type = "text"
+    renameInput.value = currentName
+
+    nameLabel.replaceWith(renameInput)
+    renameInput.focus()
+    renameInput.select()
+
+    renameInput.addEventListener('keydown', (e) => {
+        if (e.key === "Enter") {
+            saveFolderName(renameInput)
+        }
+    });
+    const handleClickOutside = (e) => {
+        if (!renameInput.parentElement.contains(e.target)) {
+            saveFolderName(renameInput)
+
+
+            document.removeEventListener('click', handleClickOutside);
+        }
+    }
+
 }
